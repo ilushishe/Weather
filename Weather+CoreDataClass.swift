52c62f47b68a93ccef6466fff5a9c849eb1f7008
ilushishe <ilushishe@gmail.com>
2020-07-13 19:49:31 -0400

    Weather dataModel created

---
+++ Weather+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Weather+CoreDataClass.swift
+//  Weather
+//
+//  Created by Ilya Kozlov on 2020-07-13.
+//  Copyright © 2020 ilushishe. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Weather)
+public class Weather: NSManagedObject {
+
+}
---
+++ Weather+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Weather+CoreDataProperties.swift
+//  Weather
+//
+//  Created by Ilya Kozlov on 2020-07-13.
+//  Copyright © 2020 ilushishe. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Weather {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Weather> {
+        return NSFetchRequest<Weather>(entityName: "Weather")
+    }
+
+    @NSManaged public var index: Int16
+    @NSManaged public var isCurrentLocation: Bool
+    @NSManaged public var cityName: String?
+    @NSManaged public var weatherDescription: String?
+    @NSManaged public var descrtiptionCode: String?
+    @NSManaged public var temp_c: Float
+    @NSManaged public var temp_f: Float
+    @NSManaged public var feels_c: Float
+    @NSManaged public var feels_f: Float
+    @NSManaged public var last_updated: Date?
+
+}

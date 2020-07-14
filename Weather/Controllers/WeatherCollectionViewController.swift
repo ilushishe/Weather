586c3f5b0dedd8aac9400ef015cebe8ec980c27a
ilushishe <ilushishe@gmail.com>
2020-07-14 09:09:23 -0400

    WeatherCollectionViewController code structure added

---
+++ Weather/Controllers/WeatherCollectionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  WeatherCollectionViewController.swift
+//  Weather
+//
+//  Created by Ilya Kozlov on 2020-07-14.
+//  Copyright © 2020 ilushishe. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class WeatherCollectionViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - UIControls
+    
+    //MARK: - Actions
+
+
+    
+
+    // MARK: View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: Private
+private extension WeatherCollectionViewController {
+    func configureView() {
+        
+    }
+}
+
+
+// MARK: NSFetchedResultsController
+private extension WeatherCollectionViewController {
+    
+}
+
+// MARK: NSFetchedResultsControllerDelegate
+extension WeatherCollectionViewController: NSFetchedResultsControllerDelegate {
+    
+}
+
+// MARK: UICollectionViewDataSource
+extension WeatherCollectionViewController: UICollectionViewDelegate {
+    
+}
+
+

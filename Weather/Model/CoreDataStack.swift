e3413f7bf1205eeea5b38d29d48f82a7ca12951d
ilushishe <ilushishe@gmail.com>
2020-07-14 08:35:51 -0400

    CoreDataStack class created

---
+++ Weather/Model/CoreDataStack.swift
@@ -0,0 +1,38 @@
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    //MARK: - properties
+    private let modelName: String
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    private lazy var storeContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    //MARK: - methods
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    func saveContext () {
+        guard managedContext.hasChanges else { return }
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
